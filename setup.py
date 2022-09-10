13df63079d8eef1285f26a8eb8e13d356a99668e
w4ffl35 <25737761+w4ffl35@users.noreply.github.com>
2022-09-10 11:12:43 -0600

    Adds initial setup.py file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="stablediffusiond",
+    version="0.1.0",
+    description="A simple daemon which listens for requests on a queue, runs stable diffusion scripts and enqueues the results.",
+    url="https://github.com/w4ffl35/stablediffusiond.git",
+    author="w4ffl35 (Joe Curlee)",
+    author_email="25737761+w4ffl35@users.noreply.github.com",
+    license="BSD-3-Clause",
+    packages=["stablediffusiond"],
+    install_requires=[
+        "pika",
+    ]
+)
